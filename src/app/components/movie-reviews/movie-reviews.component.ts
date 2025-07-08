1fcedf8f1c048254ca559d6fdd80f74194cdabf5
SaadAlShridah1 <saad.alshridah@qeema.net>
2025-07-08 18:56:59 +0300

    Added the Review feature as a component

---
+++ src/app/components/movie-reviews/movie-reviews.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-reviews',
+  imports: [],
+  templateUrl: './movie-reviews.component.html',
+  styleUrl: './movie-reviews.component.scss'
+})
+export class MovieReviewsComponent {
+
+}
