1c10f0b86c572bec55a98f2a776c9710d0366d1f
SaadAlShridah1 <saad.alshridah@qeema.net>
2025-07-10 00:21:19 +0300

    Create language selector component

---
+++ src/app/components/language-selector/language-selector.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LanguageService } from '../../services/language.service';
+
+@Component({
+  selector: 'app-language-selector',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './language-selector.component.html',
+  styleUrl: './language-selector.component.scss'
+})
+export class LanguageSelectorComponent {
+  languageService = inject(LanguageService);
+
+  onLanguageChange(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    const selectedLanguage = target.value;
+    
+    this.languageService.setLanguage(selectedLanguage);
+    
+    window.location.reload();
+  }
+}
