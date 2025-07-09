0048f8ca21d5886c668f5aa693e0234db598617b
SaadAlShridah1 <saad.alshridah@qeema.net>
2025-07-09 21:47:00 +0300

    add Search component to application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { MoviesListComponent } from './components/movies-list/movies-list.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { SearchComponent } from './components/search/search.component';
+
 
 export const routes: Routes = [
   { path: '', component: MoviesListComponent },
   { path: 'movie/:id', component: MovieDetailsComponent },
+    { path: 'wishlist', component: WishlistComponent },
+      { path: 'search', component: SearchComponent },
   { path: '**', redirectTo: '' }
 ];
